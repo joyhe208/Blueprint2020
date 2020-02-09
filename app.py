8b088f0fe3fbfb5dcfa61decdb65fa5f3c072c53
Alek Westover <alek.westover@gmail.com>
2020-02-09 08:48:37 -0500

    iniit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+import json
+app = Flask(__name__)
+
+acceptingBuzzes = True
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
