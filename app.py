218811439d6938eaee63408cca53d8c37935f3d6
Rishi Sinha <Rishi.Sinha100@gmail.com>
2020-02-09 10:26:34 -0500

    Made some functions

--- app.py
+++ app.py
@@ -11,3 +11,36 @@ def index():
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
 
+
+def create_task():
+    data["tasks"]=[]
+    with open ('tasks.json', 'w') as outfile: 
+        json.dump(data, outfile)
+
+    
+def verify_task_complete():
+    with open('tasks.json') as json_file:
+        tasks = json.load(json_file)
+        for task in tasks:
+            if task['initiatior'] == user: 
+                reward = task["reward"]
+            
+
+
+
+     
+
+
+
+def authenticate_login():
+    with open('users.json') as json_file:
+        users = json.load(json_file)
+        for user in users: 
+            if name == user: 
+                if data['user']== password: 
+                    return "Success"
+                else: 
+                    return "Password incorrect"
+            else: 
+                return("Username not found!")
+         
